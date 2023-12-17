79146a38c30b6568cfad1e5b25942ea626fabb8f
shrutiiiiin <shrutinaik2317@gmail.com>
2023-12-17 22:12:56 +0530

    initial commit#4

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
-      backgroundColor: Colors.yellow[200],
+      backgroundColor: Colors.blue[200],
       appBar: AppBar(
         title: Text(
           'Dice',
           textAlign: TextAlign.center,
         ),
-        backgroundColor: Colors.yellow[50],
+        backgroundColor: Colors.blue[100],
       ),
       body: DicePage(),
     ),
@@ -21,14 +22,34 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // int left = 1;
+    // int right = 1;
+    // void random() {
+    //   left = Random().nextInt(6) + 1;
+    //   right = Random().nextInt(6) + 1;
+    // }
+
     return Center(
       child: Row(
         children: [
           Expanded(
-            child: Image.asset('images/dice1.png'),
+            child: TextButton(
+              child: Image.asset('images/dice$left.png'),
+              onPressed: () {
+                // setState(
+                //   random();
+                print('right button pressed');
+                )
+              },
+            ),
           ),
           Expanded(
-            child: Image.asset('images/dice1.png'),
+            child: TextButton(
+              child: Image.asset('images/dice$right.png'),
+              onPressed: () {
+                print('right button pressed');
+              },
+            ),
           ),
         ],
       ),
